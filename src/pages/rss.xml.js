e5f247013164ec875d86fbf6a5c8a7048cd56fbc
Vladimir Plesco <pleshco@deeplace.md>
2025-01-03 15:29:35 +0200

    next attempt

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,7 @@
 // import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import { sortPosts} from "../../scripts/helpers";
 export async function GET(context) {
   const posts = sortPosts(await getCollection('blog'));
   return rss({
